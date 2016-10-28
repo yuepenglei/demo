7d73a91e7d34a08dd65569589d7cbfddbd5e66d0
yuepenglei <yuepenglei@yuepenglei-PC.intranet.lyarc.com>
2016-10-28 13:50:43 +0800

    项目框架雏形

---
+++ src/main/java/com/lyarc/mavenStudy/App.java
@@ -0,0 +1,13 @@
+package com.lyarc.mavenStudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
